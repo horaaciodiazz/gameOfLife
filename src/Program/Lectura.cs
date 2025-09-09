a228b3b820d56e4711bcf03648a82c82d6bbf2ae
Andres <ANDRES.RODRIGUEZC@correo.ucu.edu.uy>
2025-09-09 10:55:44 -0300

    cambioooo

--- src/Program/Lectura.cs
+++ src/Program/Lectura.cs
@@ -1,6 +1,26 @@
 namespace Ucu.Poo.GameOfLife;
 
+using System.IO;
+
 public class Lectura
 {
-    
+    public Tablero Leer(string url)
+    {
+        string content = File.ReadAllText(url);
+        string[] contentLines = content.Split('\n');
+        
+        Tablero tablero = new Tablero(contentLines.Length, contentLines[0].Length);
+        for (int y = 0; y < contentLines.Length; y++)
+        {
+            for (int x = 0; x < contentLines[y].Length; x++)
+            {
+                if (contentLines[y][x] == '1')
+                {
+                    tablero.CambiarCelula(x, y, true);
+                }
+            }
+        }
+
+        return tablero;
+    }
 }
